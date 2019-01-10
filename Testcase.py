beb95b904458129b9997101238f234dff90bd507
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-10 20:15:41 +0530

    Loops

---
+++ Testcase.py
@@ -0,0 +1,19 @@
+a = int(input())
+b = int(input())
+c = int(input())
+print("enter the input")
+if a>b and a>c:
+    print("a is largest")
+if b>a and b>c:
+    print("resut")
+if c>a and c>b:
+    print("c is largest")
+
+    #for loop
+
+n = int(input("Enter the number of rows you want to print?"))
+i, j = 0, 0
+for i in range(0, n):
+ print("hello")
+for j in range(0, i + 1):
+ print("*", end="")
